7f5bcd9d067c476d4ebbbe25f89f5b62c9e86beb
unknown <519844502@qq.com>
2018-08-14 15:45:40 +0800

    替换动画完成 2018年8月14日15:45:37

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+    // 导航栏
     $(".header_main_li").mouseenter(function(){
         $(this).children(".header_main_lidropdown").fadeIn()
     });
     $(".header_main_li").mouseleave(function(){
         $(this).children(".header_main_lidropdown").fadeOut()
     });
+
+    //新品首发图片替换
+
 })
